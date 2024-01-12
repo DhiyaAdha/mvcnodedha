f80e953d04e495b6a38d6e3c3521c8462714bb25
Dhiya'Uddin Adha S <lJIMM23002@pamapersada.net>
2024-01-13 02:23:58 +0700

    crud mvc final #4.2

--- main.js
+++ main.js
@@ -10,12 +10,15 @@ const mysql = require('mysql2');
 const sequelize = require('./db-config.js');
 // console.log(sequelize);
 
-// const session = require('express-session');
-
 //2. port express
 const app = express();
 const PORT = process.env.PORT || 4000;
 
+/**
+ * helpers handlebars express
+ * pengguanan format handlebars
+ * pengurutan nomor
+ */
 // Set up handlebars as the template engine
 app.engine(
     'handlebars',
@@ -30,6 +33,10 @@ app.engine(
     })
   );
 app.set('view engine', 'handlebars');
+
+const handlebars = require('handlebars');
+const helpers = require('handlebars-helpers')();
+handlebars.registerHelper(helpers);
 
 // Middleware untuk mengakses req.body
 app.use(express.urlencoded({ extended: true }));
@@ -52,86 +59,10 @@ app.get('/welcome', (req, res) => {
 const userRouter = require('./routes/router-user');
 app.use('/', userRouter);
 
-
-const handlebars = require('handlebars');
-const helpers = require('handlebars-helpers')();
-
-handlebars.registerHelper(helpers);
-
-// app.get('/users', async  (req, res) => {
-//     try {
-//         const users = await sequelize.query('SELECT * FROM user ORDER BY id DESC', { type: sequelize.QueryTypes.SELECT });
-//         console.log(users);
-//         res.render('index', { 
-//             title: 'Pages User',
-//             subTitle: 'Page Users',
-//             additionalInfo: 'Data Monitoring User',
-//             users: users,
-//         });
-//     } catch (error) {
-//         console.error('Error fetching users:', error);
-//         res.status(500).json({ error: 'Internal Server Error' });
-//     }
-// });
-
-/*
-*fungsi router awal(1)
-app.get('/users', async  (req, res) => {
-    try {
-        const users = await sequelize.query('SELECT * FROM user ORDER BY id DESC', { type: sequelize.QueryTypes.SELECT });
-        console.log(users);
-        res.render('index', { 
-            title: 'Pages User',
-            subTitle: 'Page Users',
-            additionalInfo: 'Data Monitoring User',
-            users: users,
-        });
-    } catch (error) {
-        console.error('Error fetching users:', error);
-        res.status(500).json({ error: 'Internal Server Error' });
-    }
-});
-
-// page create
-app.get('/create_users', (req, res) => {
-    res.render('create', { 
-        title: 'Page Create',
-        subTitle: 'Page Create',
-        additionalInfo: 'Tambah User',
-    });
-});
-
-// pages detail
-app.get('/detail_users', (req, res) => {
-    res.render('detail_update', { 
-        title: 'Page Details',
-        subTitle: 'Page Detail',
-        additionalInfo: 'Detail & Update User', });
-});
-
-*/
-
 // Port
 app.listen(PORT, () => {
     console.log(`Server is running on http://localhost:${PORT}`);
 });
 
-// Check database
-// console.log(mysql);
-/*
-fungsi migration & seeder
-automation input data
-dengan sequelize 
-*/
 
-// sequelize.sync()
-//   .then(() => {
-//     console.log('Database and tables are in sync.');
-//   })
-//   .catch((err) => {
-//     console.error('Error syncing database:', err);
-//   });
-
-// Gunakan userController sebagai middleware
-// app.use('/users', userController);
-
+//finis v1
